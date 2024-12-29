092aef8c25010c9ebd0d0a526ae6e582d83201bd
volodymyrd <volodymyrdotsenko@gmail.com>
2024-12-29 01:04:04 +0100

    add show more, fox index out of bound, add previous.

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,17 +1,32 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import {sculptureList} from '../data/data';
 
 export default function Gallery() {
     const [index, setIndex] = useState(0);
+    const [showMore, setShowMore] = useState(false);
 
-    function handleClick() {
+    let hasPrev = index > 0;
+    let hasNext = index < sculptureList.length - 1;
+
+    function handleNextClick() {
         setIndex(index + 1);
+    }
+
+    function handlePrevClick() {
+        setIndex(index - 1);
+    }
+
+    function handleMoreClick() {
+        setShowMore(!showMore);
     }
 
     let sculpture = sculptureList[index];
     return (
         <>
-            <button onClick={handleClick}>
+            <button onClick={handlePrevClick} disabled={!hasPrev}>
+                Previous
+            </button>
+            <button onClick={handleNextClick} disabled={!hasNext}>
                 Next
             </button>
             <h2>
@@ -21,13 +36,14 @@ export default function Gallery() {
             <h3>
                 ({index + 1} of {sculptureList.length})
             </h3>
+            <button onClick={handleMoreClick}>
+                {showMore ? 'Hide' : 'Show'} details
+            </button>
+            {showMore && <p>{sculpture.description}</p>}
             <img
                 src={sculpture.url}
                 alt={sculpture.alt}
             />
-            <p>
-                {sculpture.description}
-            </p>
         </>
     );
 }
